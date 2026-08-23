ddde89a4c9c99f577c49050a5c722895e2404d55
Oles Slava <oles.slava23@gmail.com>
2026-08-23 13:53:15 +0300

    Updates

--- src/components/Choice/Choice.styled.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const CohiceStyle = styled.h1`
-  font-size: 46px;
-  font-weight: 800;
-  line-height: 52px;
-`
--- src/components/StickersList/StykersList.styled.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const StickerListStyle = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 16px;
-`
